dac838cb141eb18358eca5b37d533a1f1784d6c9
Mahesh Nair <maheshnair73@gmail.com>
2026-05-20 08:08:02 +0530

    Deleted PlanGate.tsx

--- dist/assets/chevron-right-BPut-wXs.js
+++
@@ -1,6 +0,0 @@
-import{i as t}from"./index-rZpY6uNV.js";/**
- * @license lucide-react v0.368.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const h=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C};
